008f28b3c387007dcb8534b982da4935027d9605
Helen Nguyen <hnguye89@uncc.edu>
2023-10-15 23:14:31 -0400

    Add connection from MongoDB

--- server.js
+++ server.js
@@ -5,6 +5,39 @@ const port = 4000;
 const mongoDBClient = require('mongodb').MongoClient; 
 let url = 'mongodb://localhost27017/personal_budget'; 
 
+//CRUD = Create - Read - Update - Delete 
+
+// 1 - connect to database 
+// 2 - perform the desired operation
+// 3 - close database connection 
+mongoDBClient.connect(url, {useUnifiedTopology: true}, (operationError, dbHandler)=>{
+    if(operationError){
+        console.log("An error has occured during the connection process"); 
+    } else{
+        console.log("Connected to the database");
+        let data = {id: 10, name: "added a new name from MongoDB Native Driver"};
+        dbHandler.db("personal_budget").collection('names').insertOne(data, (operr, opresult)=>{
+            if(operr){
+                console.log("Unable to insert data into your collection");
+            }else{
+                console.log("Insert Successfully");
+                //dbHandler.close(); 
+            }
+        })
+        //List Operation
+        dbHandler.db('personal_budget').collection('names').find().toArray((operr, opresult)=>{
+            if(operr){
+                console.log(operr); 
+            } else{
+                for(var i=0; i < opresult.length; i++){
+                    console.log(opresult[i]); 
+                }
+                dbHandler.close(); 
+            }
+        }); 
+    }
+});
+
 app.use('/', express.static('public'));
 
 const budget = {
